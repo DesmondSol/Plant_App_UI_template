a0d7b710fed66b9751e932acbcd44bd6f188fa99
desmondsol <soltig66@gmail.com>
2021-07-25 18:11:40 +0300

    new pages added- sell waste

---
+++ app/src/main/java/com/project/plantappui/sell_Waste_for_recycling.java
@@ -0,0 +1,14 @@
+package com.project.plantappui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sell_Waste_for_recycling extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_waste_for_recycling);
+    }
+}
