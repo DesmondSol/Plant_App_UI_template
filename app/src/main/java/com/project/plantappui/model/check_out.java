9b1312622aa40fca0660f1280fe0468ac7ff8d0e
desmondsol <soltig66@gmail.com>
2021-07-25 15:31:30 +0300

    new pages added- e commerce page

---
+++ app/src/main/java/com/project/plantappui/model/check_out.java
@@ -0,0 +1,16 @@
+package com.project.plantappui.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.plantappui.R;
+
+public class check_out extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out);
+    }
+}
---
+++ app/src/main/java/com/project/plantappui/ui/login/RegisterActivity.java
@@ -0,0 +1,6 @@
+package com.project.plantappui.ui.login;
+
+import android.app.Activity;
+
+public class RegisterActivity extends Activity {
+}
